b05a0c6acd4642cb97b490962b5f0fb6d549db8f
Daniel Richards <daniel.richards@automattic.com>
2020-01-29 16:08:31 +0800

    Remove left/right arrow key looping behaviour

--- packages/block-editor/src/components/tree-grid/index.js
+++ packages/block-editor/src/components/tree-grid/index.js
@@ -64,9 +64,9 @@ export default function TreeGrid( { children, ...props } ) {
 			// Cycle to the next element and focus it.
 			let nextIndex;
 			if ( keyCode === LEFT ) {
-				nextIndex = ( currentColumnIndex - 1 ) < 0 ? focusablesInRow.length - 1 : currentColumnIndex - 1;
+				nextIndex = ( currentColumnIndex - 1 ) < 0 ? 0 : currentColumnIndex - 1;
 			} else {
-				nextIndex = ( currentColumnIndex + 1 ) >= focusablesInRow.length ? 0 : currentColumnIndex + 1;
+				nextIndex = ( currentColumnIndex + 1 ) >= focusablesInRow.length ? focusablesInRow.length - 1 : currentColumnIndex + 1;
 			}
 
 			focusablesInRow[ nextIndex ].focus();
